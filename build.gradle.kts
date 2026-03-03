76d12fe0e7665242c2da4d1d5054661f205b743c
RettichLP <rettichlp01@gmail.com>
2026-03-03 03:08:55 +0100

    Update build scripts and dependencies, streamline workflows, and remove unnecessary custom tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 import org.gradle.api.JavaVersion.VERSION_25
-import xyz.jpenilla.runpaper.task.RunServer
 
 group = "de.rettichlp.therettingtonconcierge"
 version = project.findProperty("trcVersion") as? String ?: "0.0.0"
-println("> The-Rettington-Concierge-Version: $version")
+println("> The Rettington Concierge Version: $version")
 
 java {
     sourceCompatibility = VERSION_25
@@ -18,8 +17,8 @@ plugins {
     `maven-publish`
 
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("com.gradleup.shadow") version "9.3.2"
     id("xyz.jpenilla.run-paper") version "3.0.2"
-    id("com.gradleup.shadow") version "9.3.1"
 }
 
 repositories {
@@ -27,24 +26,19 @@ repositories {
     mavenCentral()
 
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
+    api("io.papermc.paper", "paper-api", "1.21.11-R0.1-SNAPSHOT")
+    api("net.kyori", "adventure-text-serializer-ansi", "4.26.1")
+
     // https://mvnrepository.com/artifact/org.atteo.classindex/classindex
     api("org.atteo.classindex", "classindex", "3.13")
     annotationProcessor("org.atteo.classindex", "classindex", "3.13")
-    testAnnotationProcessor("org.atteo.classindex", "classindex", "3.13")
-
-    api("io.papermc.paper", "paper-api", "1.21.11-R0.1-SNAPSHOT")
-    api("net.kyori", "adventure-text-serializer-ansi", "4.26.1")
-    testCompileOnly("io.papermc.paper", "paper-api", "1.21.11-R0.1-SNAPSHOT")
 
     // https://mvnrepository.com/artifact/org.projectlombok/lombok
     compileOnly("org.projectlombok", "lombok", "1.18.42")
     annotationProcessor("org.projectlombok", "lombok", "1.18.42")
-    testCompileOnly("org.projectlombok", "lombok", "1.18.42")
-    testAnnotationProcessor("org.projectlombok", "lombok", "1.18.42")
 
     // https://mvnrepository.com/artifact/com.google.inject/guice
     implementation("com.google.inject", "guice", "7.0.0")
@@ -53,13 +47,13 @@ dependencies {
     implementation("com.google.code.gson", "gson", "2.13.2")
 
     // https://mvnrepository.com/artifact/org.springframework/spring-webflux
-    implementation("org.springframework", "spring-webflux", "7.0.4")
+    implementation("org.springframework", "spring-webflux", "7.0.5")
 
     // https://mvnrepository.com/artifact/org.springframework.data/spring-data-mongodb
     implementation("org.springframework.data", "spring-data-mongodb", "5.0.3")
 
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-yaml
-    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.21.0")
+    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.21.1")
 
     paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
 }
@@ -77,47 +71,8 @@ tasks {
         isFailOnError = false
     }
 
-    register<Jar>("createTestRunPaperJar") {
-        mustRunAfter(shadowJar)
-
-        // Include everything from the shadowJar
-        from({ zipTree(shadowJar.get().archiveFile.get().asFile) })
-
-        // Add test sources and resources
-        from(sourceSets.test.get().output)
-        from(sourceSets.test.get().resources)
-
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-        doLast {
-            println("> Test Run-Paper: Build custom test run jar")
-        }
-
-        group = "Custom Build Tasks"
-    }
-
-    // Task to copy the final JAR to run/plugins
-    register<Copy>("copyTestRunPaperJar") {
-        dependsOn("createTestRunPaperJar")
-
-        from(named<Jar>("createTestRunPaperJar").flatMap { it.archiveFile })
-        into(layout.projectDirectory.dir("run/plugins")) // Copy to run/plugins directory
-
-        doLast {
-            println("> Test Run-Paper: Copied custom-test-run-paper.jar to run/plugins/")
-        }
-
-        group = "Custom Build Tasks"
-    }
-
-    withType<RunServer> {
-        dependsOn(shadowJar)
-        dependsOn("createTestRunPaperJar")
-        dependsOn("copyTestRunPaperJar")
-    }
-
-    test {
-        useJUnitPlatform()
+    jar {
+        dependsOn("shadowJar")
     }
 }
 

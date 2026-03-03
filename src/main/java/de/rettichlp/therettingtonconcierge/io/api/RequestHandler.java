25263f478ba0bfc28f97e742e89cba6f949ffed0
RettichLP <rettichlp01@gmail.com>
2026-03-03 02:57:13 +0100

    Simplify Javadoc comments in RequestHandler and its nested classes

--- src/main/java/de/rettichlp/therettingtonconcierge/io/api/RequestHandler.java
+++ src/main/java/de/rettichlp/therettingtonconcierge/io/api/RequestHandler.java
@@ -28,66 +28,51 @@ public class RequestHandler {
 
     /**
      * Creates a {@link GetRequest} object to perform an HTTP GET request for a specified resource.
-     * <p>
-     * This method is designed to encapsulate the details for an HTTP GET request, including the URL of the resource and the expected
-     * type of the response data. The created {@link GetRequest} object allows further customization and execution of the GET request
-     * and contains the logic to deserialize the JSON response into an object of the specified type.
      *
      * @param <T>        The type of the object expected as the response from the GET request.
      * @param url        The endpoint URL for the GET request.
      * @param modelClass The class type of the model to deserialize the JSON response into.
      *
-     * @return A {@link GetRequest} object initialized with the given URL and model class, which can be used to execute the GET request
-     *         and retrieve the response.
+     * @return A {@link GetRequest} object initialized with the given URL and model class.
      */
     public <T> GetRequest<T> get(String url, Class<T> modelClass) {
         return new GetRequest<>(url, modelClass);
     }
 
     /**
-     * Sends an HTTP PUT request to the specified URL with the provided model as the payload.
-     * <p>
-     * This method constructs a {@link PutRequest} object to represent the HTTP PUT request. The model is serialized into JSON format
-     * and included in the request body. The created {@link PutRequest} object can then be executed synchronously or asynchronously.
+     * Creates a {@link PutRequest} object to perform an HTTP PUT request for a specified resource.
      *
-     * @param <T>   The type of the model to be sent as the request body.
-     * @param url   The endpoint URL to which the PUT request is sent.
+     * @param <T>   The type of the object to be sent as the request body.
+     * @param url   The endpoint URL for the PUT request.
      * @param model The data model to be serialized and included in the PUT request.
      *
-     * @return A {@link PutRequest} object that encapsulates the details of the PUT request.
+     * @return A {@link PutRequest} object initialized with the given URL and model.
      */
     public <T> PutRequest<T> put(String url, T model) {
         return new PutRequest<>(url, model);
     }
 
     /**
-     * Sends an HTTP POST request to the specified URL with the provided model as the payload.
-     * <p>
-     * This method constructs a {@link PostRequest} object to represent the HTTP POST request. The model is serialized into JSON format
-     * and included in the request body. The created {@link PostRequest} object can then be executed synchronously or asynchronously.
+     * Creates a {@link PostRequest} object to perform an HTTP POST request for a specified resource.
      *
-     * @param <T>   The type of the model to be sent as the request body.
-     * @param url   The endpoint URL to which the POST request is sent.
+     * @param <T>   The type of the object to be sent as the request body.
+     * @param url   The endpoint URL for the POST request.
      * @param model The data model to be serialized and included in the POST request.
      *
-     * @return A {@link PostRequest} object that encapsulates the details of the POST request.
+     * @return A {@link PostRequest} object initialized with the given URL and model.
      */
     public <T> PostRequest<T> post(String url, T model) {
         return new PostRequest<>(url, model);
     }
 
     /**
-     * Sends an HTTP DELETE request to the specified URL with the given model as the payload.
-     * <p>
-     * The model is serialized into JSON format and included in the request body. The DELETE request is constructed using the provided
-     * URL and model. This method is designed to return a {@link DeleteRequest} object representing the configured request, which can
-     * be executed synchronously or asynchronously.
+     * Creates a {@link DeleteRequest} object to perform an HTTP DELETE request for a specified resource.
      *
-     * @param <T>   The type of the model to be sent as the request body.
-     * @param url   The endpoint URL to which the DELETE request is sent.
+     * @param <T>   The type of the object to be sent as the request body.
+     * @param url   The endpoint URL for the DELETE request.
      * @param model The data model to be serialized and included in the DELETE request.
      *
-     * @return A {@link DeleteRequest} object that encapsulates the details of the DELETE request.
+     * @return A {@link DeleteRequest} object initialized with the given URL and model.
      */
     public <T> DeleteRequest<T> delete(String url, T model) {
         return new DeleteRequest<>(url, model);
@@ -127,10 +112,10 @@ public class RequestHandler {
     }
 
     /**
-     * Represents an HTTP GET request for retrieving and deserializing JSON data into a specific model class. This class supports
-     * operations to fetch a single object or a list of objects using a specified URL and deserialization type.
+     * Represents an HTTP GET request that retrieves data from a specified URL and deserializes the response into objects of a
+     * specified type.
      *
-     * @param <T> The type of object that the JSON response is deserialized into.
+     * @param <T> The type of object that the JSON response will be deserialized into.
      */
     @Getter
     @RequiredArgsConstructor
@@ -140,25 +125,20 @@ public class RequestHandler {
         private final Class<T> modelClass;
 
         /**
-         * Retrieves and deserializes a single object of type {@code T} from a JSON response received via an HTTP GET request.
-         * <p>
-         * The method sends a GET request to the specified URL and deserializes the response body into an object of the type
-         * represented by {@code modelClass}. Uses Gson for JSON deserialization.
+         * Retrieves a resource from the specified URL via an HTTP GET request and deserializes the JSON response into an object of the
+         * type specified by the {@code modelClass} field.
          *
-         * @return an instance of type {@code T}, deserialized from the JSON response returned by the GET request
+         * @return An instance of type {@code T} representing the deserialized response data.
          */
         public T retrieve() {
             return GSON.fromJson(sendRequest(this.url, GET, null), this.modelClass);
         }
 
         /**
-         * Sends an HTTP GET request to the specified URL and deserializes the JSON response into a list of objects of the specified
-         * model class type.
-         * <p>
-         * The method uses Gson for JSON deserialization and generics to infer the type of the resulting list. It assumes the JSON
-         * response from the HTTP GET request contains an array of objects that match the structure of the specified model class.
+         * Retrieves a resource from the specified URL via an HTTP GET request and deserializes the JSON response into a list of
+         * objects of the type specified by the {@code modelClass} field.
          *
-         * @return a list of objects of type {@code T}, deserialized from the JSON response.
+         * @return A {@code List<T>} representing the deserialized response data.
          */
         public List<T> retrieveAsList() {
             return GSON.fromJson(sendRequest(this.url, GET, null), getParameterized(List.class, this.modelClass).getType());
@@ -166,14 +146,9 @@ public class RequestHandler {
     }
 
     /**
-     * Represents a generic HTTP POST request.
-     * <p>
-     * This class is designed to handle HTTP POST requests by sending a JSON payload derived from a provided model object. The URL
-     * endpoint and model are specified during object instantiation.
-     * <p>
-     * It provides methods for sending the POST request both synchronously and asynchronously.
+     * Represents an HTTP POST request used to send data to a specified URL with the request body serialized in JSON format.
      *
-     * @param <T> The type of the model object to be serialized into a JSON payload and sent in the request body.
+     * @param <T> The type of the model object to be serialized and included in the HTTP POST request.
      */
     @Getter
     @RequiredArgsConstructor
@@ -182,73 +157,23 @@ public class RequestHandler {
         private final String url;
         private final T model;
 
-        /**
-         * Sends a POST HTTP request to the specified URL with the model serialized in JSON format.
-         * <p>
-         * The method utilizes the URL and model provided during the instantiation of the PostRequest. It serializes the model object
-         * to JSON using the Gson instance, and sends the request using the HTTP POST method.
-         * <p>
-         * This method blocks execution until the request is completed.
-         * <p>
-         * Note: Uses the {@code sendRequest} method of the containing class to perform the HTTP request.
-         */
         public void send() {
             sendRequest(this.url, POST, GSON.toJson(this.model));
         }
 
-        /**
-         * Sends the current request asynchronously.
-         * <p>
-         * This method serializes the {@code model} object to a JSON string using Gson and sends it to the specified {@code url} using
-         * an asynchronous HTTP POST request. The request is processed without blocking the calling thread.
-         * <p>
-         * Responsibilities: - Converts the {@code model} into a JSON representation. - Dispatches the JSON payload to the specified
-         * {@code url} via a POST request. - Leverages an asynchronous communication mechanism.
-         * <p>
-         * Use Cases: - Suitable for scenarios where the result of the HTTP request does not need to be immediately available to the
-         * calling code.
-         * <p>
-         * Limitations: - The method does not provide a direct mechanism to handle the response or errors. If needed, the
-         * implementation of {@code sendRequestAsync} should be customized to include appropriate error-handling logic or hooks for
-         * response processing.
-         * <p>
-         * Dependencies: - Relies on the {@code sendRequestAsync} method of the {@link RequestHandler} class to perform the
-         * asynchronous HTTP POST communication. - Utilizes the Gson serialization framework to convert the {@code model} into JSON
-         * format.
-         */
         public void sendAsync() {
             sendRequestAsync(this.url, POST, GSON.toJson(this.model));
         }
 
-        /**
-         * Retrieves a deserialized instance of the specified model class by sending an HTTP POST request to the associated URL with
-         * the serialized JSON representation of the current model object. The method blocks until the request completes and processes
-         * the response by deserializing it into an instance of the given model class.
-         *
-         * @param modelClass the class type of the model to deserialize the response into
-         *
-         * @return an instance of the specified model class populated with the data from the HTTP response
-         */
         public T retrieve(Class<T> modelClass) {
             return GSON.fromJson(sendRequest(this.url, POST, GSON.toJson(this.model)), modelClass);
         }
     }
 
     /**
-     * Represents a generic PUT HTTP request that encapsulates a target URL and a model object. This class allows sending HTTP PUT
-     * requests synchronously or asynchronously with the JSON representation of the provided model object.
-     * <p>
-     * The PutRequest class is parameterized to accept a model of any type. The model is serialized into JSON format using Gson before
-     * the request is sent.
-     * <p>
-     * Responsibilities: - Stores the URL and the model object to be sent in the PUT request. - Provides functionality for sending the
-     * request either synchronously or asynchronously. - Utilizes utility methods to handle the HTTP request and JSON serialization.
-     * <p>
-     * Usage Notes: - Instances of this class are typically created using a factory or builder pattern. - The URL and model object are
-     * immutable once the PutRequest object is constructed. - The class depends on an external Gson instance for JSON serialization. -
-     * Error handling or response processing is expected to be implemented at a higher level.
+     * Represents an HTTP PUT request used to send data to a specified URL with the request body serialized to JSON format.
      *
-     * @param <T> The type of the model object to be sent in the PUT request.
+     * @param <T> The type of the model object to be serialized and included in the HTTP PUT request.
      */
     @Getter
     @RequiredArgsConstructor
@@ -257,64 +182,19 @@ public class RequestHandler {
         private final String url;
         private final T model;
 
-        /**
-         * Sends a synchronous HTTP PUT request to the specified URL with the serialized JSON representation of the associated model
-         * object.
-         * <p>
-         * This method prepares the JSON payload by serializing the {@code model} field using a Gson instance and uses the
-         * {@code sendRequest} utility method for executing the request. The HTTP request is blocking, meaning it is executed in a
-         * synchronous manner and does not return a result to the caller.
-         * <p>
-         * Responsibilities: - Serializes the {@code model} object into JSON format using Gson. - Sends the JSON data to the URL using
-         * a PUT HTTP request.
-         * <p>
-         * Notes: - Callers must ensure that the {@code url} property and {@code model} object are correctly set before invoking this
-         * method. - This method does not handle the response or provide feedback about the success or failure of the request.
-         */
         public void send() {
             sendRequest(this.url, PUT, GSON.toJson(this.model));
         }
 
-        /**
-         * Asynchronously sends a PUT HTTP request to the specified URL with the serialized JSON representation of the current model
-         * object. This method utilizes a non-blocking approach to perform the request and does not return any result directly.
-         * <p>
-         * The method uses the {@code sendRequestAsync} utility to handle the asynchronous HTTP request. The JSON payload is
-         * constructed from the state of the {@code model} field using a Gson instance for serialization.
-         * <p>
-         * Responsibilities: - Converts the {@code model} object to its JSON representation. - Sends the converted JSON data to the URL
-         * using a PUT request. - Ensures the operation is performed asynchronously without blocking the calling thread.
-         * <p>
-         * Notes: - The success or failure of the request is handled by the subscription mechanism inside the {@code sendRequestAsync}
-         * method. - Any side effects or processing related to the response should be implemented within the subscription logic of
-         * {@code sendRequestAsync}.
-         */
         public void sendAsync() {
             sendRequestAsync(this.url, PUT, GSON.toJson(this.model));
         }
     }
 
     /**
-     * Represents an HTTP DELETE request for a specific model type.
-     * <p>
-     * This class encapsulates the details required to perform an HTTP DELETE request on a given endpoint, with the specified model
-     * object serialized as the request payload. The payload is serialized into JSON format using Gson before being sent.
-     * <p>
-     * Type Parameter:
+     * Represents an HTTP DELETE request used to send data to a specified URL with the request body serialized to JSON format.
      *
-     * @param <T> The type of the model object that will be serialized and sent as part of the DELETE request.
-     *            <p>
-     *            Responsibilities: - Stores the target URL for the HTTP DELETE request. - Holds a reference to the model object to be
-     *            serialized and sent as part of the request body. - Provides mechanisms to send the request either synchronously or
-     *            asynchronously.
-     *            <p>
-     *            Threading Considerations: - The {@code send} method performs the request synchronously, blocking the current thread
-     *            until it completes. - The {@code sendAsync} method performs the request asynchronously, allowing the calling thread
-     *            to continue execution without waiting for the request to finish.
-     *            <p>
-     *            Applications: - Use this class to remove resources from a server via HTTP DELETE requests, particularly when the
-     *            server accepts JSON payloads. It is suitable for both blocking and non-blocking operations, depending on the use
-     *            case.
+     * @param <T> The type of the model object to be serialized and included in the HTTP DELETE request.
      */
     @Getter
     @RequiredArgsConstructor
@@ -323,37 +203,10 @@ public class RequestHandler {
         private final String url;
         private final T model;
 
-        /**
-         * Sends an HTTP DELETE request with the current model serialized as a JSON payload.
-         * <p>
-         * This method performs a synchronous HTTP DELETE operation. The target URL is determined by the {@code url} field, and the
-         * JSON-serialized representation of the {@code model} field serves as the request body. The JSON serialization is handled via
-         * Gson.
-         * <p>
-         * Responsibilities: - Serializes the model field into JSON format. - Sends a DELETE request to the specified URL. - Executes
-         * the request synchronously, blocking the thread until the operation completes.
-         * <p>
-         * Note: This method should be used in scenarios where blocking behavior is acceptable and immediate confirmation of the
-         * completion of the request is required.
-         */
         public void send() {
             sendRequest(this.url, DELETE, GSON.toJson(this.model));
         }
 
-        /**
-         * Sends the DELETE request represented by this instance asynchronously.
-         * <p>
-         * This method utilizes the {@code sendRequestAsync} function to perform an asynchronous HTTP DELETE request using the provided
-         * URL and serialized payload of the model. The payload is converted to a JSON string using the Gson instance associated with
-         * this class. The request is executed on a non-blocking thread, and the calling thread is not blocked while the request is
-         * being processed.
-         * <p>
-         * Responsibilities: - Serializes the model associated with this request into JSON format. - Initiates an asynchronous HTTP
-         * DELETE request to the specified URL. - Delegates the asynchronous request execution to the {@code sendRequestAsync} method.
-         * <p>
-         * This method is particularly useful for operations where the result of the delete request is either not immediately needed or
-         * handled asynchronously, enabling higher throughput in concurrent scenarios.
-         */
         public void sendAsync() {
             sendRequestAsync(this.url, DELETE, GSON.toJson(this.model));
         }

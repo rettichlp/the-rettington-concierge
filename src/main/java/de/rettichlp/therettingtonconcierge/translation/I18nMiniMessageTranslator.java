ad93188e458b78be312a638f21e9f0dc7bb87426
RettichLP <rettichlp01@gmail.com>
2026-03-29 23:12:02 +0200

    Add multi-plugin support to I18nMiniMessageTranslator and refactor localization methods to improve translation handling

--- src/main/java/de/rettichlp/therettingtonconcierge/translation/I18nMiniMessageTranslator.java
+++ src/main/java/de/rettichlp/therettingtonconcierge/translation/I18nMiniMessageTranslator.java
@@ -10,14 +10,18 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.translation.MiniMessageTranslator;
 import net.kyori.adventure.translation.TranslationStore;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jspecify.annotations.NonNull;
 
 import java.text.MessageFormat;
+import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static java.lang.Integer.parseInt;
 import static java.util.Arrays.stream;
@@ -33,22 +37,20 @@ import static net.kyori.adventure.translation.TranslationStore.messageFormat;
 @Getter
 public class I18nMiniMessageTranslator extends MiniMessageTranslator {
 
-    @Getter
-    private static I18nMiniMessageTranslator instance;
+    private static final Collection<I18nMiniMessageTranslator> INSTANCES = new CopyOnWriteArrayList<>();
 
     private final JavaPlugin plugin;
     private final LogDispatcher logDispatcher;
-    private final String namespace;
     private final TranslationStore.@NonNull StringBased<MessageFormat> translationStore;
 
     @Inject
     public I18nMiniMessageTranslator(@NonNull JavaPlugin plugin, LogDispatcher logDispatcher) {
         this.plugin = plugin;
         this.logDispatcher = logDispatcher;
-        this.namespace = plugin.namespace();
-        this.translationStore = messageFormat(key(this.namespace + ":i18n"));
-        instance = this;
+        this.translationStore = messageFormat(key(this.plugin.namespace() + ":i18n"));
 
+        // add this translation instance to instance holder for multi-plugin-support
+        INSTANCES.add(this);
         // add this translator to the GlobalTranslator
         translator().addSource(this);
     }
@@ -75,71 +77,68 @@ public class I18nMiniMessageTranslator extends MiniMessageTranslator {
                 .orElse(null); // Returning null "ignores" this translation.
     }
 
-    private @NonNull Component localizeInstance(@NonNull String translationKey, @NonNull Locale locale, Object @NonNull ... args) {
-        // map arguments to components
-        List<@NotNull Component> argComponents = stream(args).map(TextUtils::objectToComponent).toList();
-
-        // raw string from translation store
-        String rawString = getMiniMessageString(translationKey, locale); // something like "<red>Hello <arg:0>!</red>"
-
-        if (rawString == null) {
-            this.logDispatcher.warn("Missing translation for key {} in locale {}", translationKey, locale);
-            return text(translationKey); // fallback
-        }
-
-        // create the custom mini-message builder with <arg> and <br> tag
-        MiniMessage builder = MiniMessage.builder()
-                .editTags(b -> b.tag("arg", (argumentQueue, _) -> {
-                    String value = argumentQueue.popOr("The arg tag requires exactly one argument").value();
-                    Component component = argComponents.get(parseInt(value));
-                    return inserting(component);
-                }))
-                .build();
-
-        // deserialize mini-message to component
-        return builder.deserialize(rawString);
-    }
-
-    private @NonNull List<Component> localizeMultilineInstance(@NonNull String translationKey,
-                                                               @NonNull Locale locale,
-                                                               Object @NonNull ... args) {
-        // map arguments to components
-        List<@NotNull Component> argComponents = stream(args).map(TextUtils::objectToComponent).toList();
-
-        // raw string from translation store
-        String rawString = getMiniMessageString(translationKey, locale); // something like "<red>Hello <arg:0>!</red>"
-
-        if (rawString == null) {
-            this.logDispatcher.warn("Missing translation for key {} in locale {}", translationKey, locale);
-            return singletonList(text(translationKey)); // fallback
-        }
-
-        // create the custom mini-message builder with <arg> and <br> tag
-        MiniMessage builder = MiniMessage.builder()
-                .editTags(b -> b.tag("arg", (argumentQueue, _) -> {
-                    String value = argumentQueue.popOr("The arg tag requires exactly one argument").value();
-                    Component component = argComponents.get(parseInt(value));
-                    return inserting(component);
-                }))
-                .build();
-
-        // deserialize mini-message to components
-        return stream(rawString.split("\n")).map(builder::deserialize).toList();
-    }
-
     private void loadTranslationFile(Locale locale) {
-        ResourceBundle bundle = getBundle(this.namespace, locale);
+        ResourceBundle bundle = getBundle("lang/" + locale.getLanguage(), locale, this.plugin.getClass().getClassLoader());
         this.translationStore.registerAll(locale, bundle, false);
-        this.logDispatcher.debug(getClass(), "Registered bundle {} for locale {}", this.namespace, locale);
+        this.logDispatcher.debug(getClass(), "Registered bundle {} for locale {}", this.plugin.namespace(), locale);
     }
 
     public static @NonNull Component localize(@NonNull String translationKey, @NonNull Locale locale, Object @NonNull ... args) {
-        return instance.localizeInstance(translationKey, locale, args);
+        // map arguments to components
+        List<@NotNull Component> argComponents = stream(args).map(TextUtils::objectToComponent).toList();
+
+        // raw translation from translation stores
+        Optional<String> optionalRawTranslation = getRawTranslation(translationKey, locale);
+        if (optionalRawTranslation.isEmpty()) {
+            return text(translationKey); // fallback
+        }
+
+        String rawTranslation = optionalRawTranslation.get(); // something like "<red>Hello <arg:0>!</red>"
+
+        // create the custom mini-message builder with <arg> and <br> tag
+        MiniMessage builder = getMiniMessageBuilder(argComponents);
+
+        // deserialize mini-message to component
+        return builder.deserialize(rawTranslation);
     }
 
     public static @NonNull List<Component> localizeMultiline(@NonNull String translationKey,
                                                              @NonNull Locale locale,
                                                              Object @NonNull ... args) {
-        return instance.localizeMultilineInstance(translationKey, locale, args);
+        // map arguments to components
+        List<@NotNull Component> argComponents = stream(args).map(TextUtils::objectToComponent).toList();
+
+        // raw translation from translation stores
+        Optional<String> optionalRawTranslation = getRawTranslation(translationKey, locale);
+        if (optionalRawTranslation.isEmpty()) {
+            return singletonList(text(translationKey)); // fallback
+        }
+
+        String rawTranslation = optionalRawTranslation.get(); // something like "<red>Hello <arg:0>!</red>"
+
+        // create the custom mini-message builder with <arg> and <br> tag
+        MiniMessage builder = getMiniMessageBuilder(argComponents);
+
+        // deserialize mini-message to components
+        return stream(rawTranslation.split("\n")).map(builder::deserialize).toList();
+    }
+
+    private static Optional<String> getRawTranslation(@NonNull String translationKey, @NonNull Locale locale) {
+        return INSTANCES.stream()
+                .filter(translator -> translator.getTranslationStore().canTranslate(translationKey, locale))
+                .findFirst()
+                .map(translator -> translator.getTranslationStore().translate(translationKey, locale))
+                .map(MessageFormat::toPattern);
+    }
+
+    @Contract("_ -> new")
+    private static @NonNull MiniMessage getMiniMessageBuilder(List<Component> argComponents) {
+        return MiniMessage.builder()
+                .editTags(b -> b.tag("arg", (argumentQueue, _) -> {
+                    String value = argumentQueue.popOr("The arg tag requires exactly one argument").value();
+                    Component component = argComponents.get(parseInt(value));
+                    return inserting(component);
+                }))
+                .build();
     }
 }

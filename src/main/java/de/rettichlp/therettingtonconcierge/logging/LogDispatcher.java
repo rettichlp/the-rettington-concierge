06b37a98f58a7babc8300e9def82f8da10fc8445
RettichLP <rettichlp01@gmail.com>
2026-04-18 18:24:52 +0200

    Refactor LogDispatcher to support velocity plugin logging
    Co-authored-by: AI

--- src/main/java/de/rettichlp/therettingtonconcierge/logging/LogDispatcher.java
+++ src/main/java/de/rettichlp/therettingtonconcierge/logging/LogDispatcher.java
@@ -1,5 +1,6 @@
 package de.rettichlp.therettingtonconcierge.logging;
 
+import de.rettichlp.therettingtonconcierge.registry.IMinecraftPlugin;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
@@ -7,9 +8,14 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NonNull;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
+import static java.util.logging.Level.FINE;
 import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Level.WARNING;
@@ -25,7 +31,6 @@ import static org.bukkit.Bukkit.getOnlinePlayers;
 @Builder
 public class LogDispatcher {
 
-    private Logger bukkitLogger;
     private Component prefixDebug;
     private Component prefixInfo;
     private Component prefixWarn;
@@ -34,6 +39,8 @@ public class LogDispatcher {
     private Predicate<Player> infoFilter;
     private Predicate<Player> warnFilter;
     private Predicate<Player> errorFilter;
+    private IMinecraftPlugin plugin;
+    private Logger logger;
 
     /**
      * Logs a debug message associated with a specific class, formats it with the provided arguments, and sends the message to online
@@ -46,10 +53,8 @@ public class LogDispatcher {
      */
     public void debug(@NonNull Class<?> clazz, String rawMessage, Object... args) {
         String formattedMessage = "[" + clazz.getSimpleName() + "] " + log.getMessageFactory().newMessage(rawMessage, args).getFormattedMessage();
-        this.bukkitLogger.log(INFO, "[DEBUG] " + formattedMessage);
-        getOnlinePlayers().stream()
-                .filter(player -> this.debugFilter.test(player))
-                .forEach(player -> player.sendMessage(this.prefixDebug.append(text(formattedMessage, GRAY))));
+        this.logger.log(FINE, "[DEBUG] " + formattedMessage);
+        getPlayersForLevel(FINE).forEach(player -> player.sendMessage(this.prefixDebug.append(text(formattedMessage, GRAY))));
     }
 
     /**
@@ -61,10 +66,8 @@ public class LogDispatcher {
      */
     public void info(String rawMessage, Object... args) {
         String formattedMessage = log.getMessageFactory().newMessage(rawMessage, args).getFormattedMessage();
-        this.bukkitLogger.log(INFO, formattedMessage);
-        getOnlinePlayers().stream()
-                .filter(player -> this.infoFilter.test(player))
-                .forEach(player -> player.sendMessage(this.prefixInfo.append(text(formattedMessage, GRAY))));
+        this.logger.log(INFO, formattedMessage);
+        getPlayersForLevel(INFO).forEach(player -> player.sendMessage(this.prefixInfo.append(text(formattedMessage, GRAY))));
     }
 
     /**
@@ -76,10 +79,8 @@ public class LogDispatcher {
      */
     public void warn(String rawMessage, Object... args) {
         String formattedMessage = log.getMessageFactory().newMessage(rawMessage, args).getFormattedMessage();
-        this.bukkitLogger.log(WARNING, formattedMessage);
-        getOnlinePlayers().stream()
-                .filter(player -> this.warnFilter.test(player))
-                .forEach(player -> player.sendMessage(this.prefixWarn.append(text(formattedMessage, GRAY))));
+        this.logger.log(WARNING, formattedMessage);
+        getPlayersForLevel(WARNING).forEach(player -> player.sendMessage(this.prefixWarn.append(text(formattedMessage, GRAY))));
     }
 
     /**
@@ -116,11 +117,30 @@ public class LogDispatcher {
      * @param message   the error message to log and display to players
      */
     public void error(Throwable throwable, String message) {
-        this.bukkitLogger.log(SEVERE, message, throwable);
-        getOnlinePlayers().stream()
-                .filter(player -> this.errorFilter.test(player))
-                .forEach(player -> player.sendMessage(this.prefixError.append(text(message, GRAY))
-                        .hoverEvent(getStackTraceComponent(throwable))));
+        this.logger.log(SEVERE, message, throwable);
+        getPlayersForLevel(SEVERE).forEach(player -> player.sendMessage(this.prefixError.append(text(message, GRAY))
+                .hoverEvent(getStackTraceComponent(throwable))));
+    }
+
+    private List<? extends Player> getPlayersForLevel(Level level) {
+        if (!this.plugin.isPaperPlugin()) {
+            return List.of();
+        }
+
+        Collection<? extends Player> onlinePlayers = getOnlinePlayers();
+        Stream<? extends Player> filteredPlayersStream = switch (level.getName()) {
+            case "DEBUG" -> onlinePlayers.stream()
+                    .filter(player -> this.debugFilter.test(player));
+            case "INFO" -> onlinePlayers.stream()
+                    .filter(player -> this.infoFilter.test(player));
+            case "WARNING" -> onlinePlayers.stream()
+                    .filter(player -> this.warnFilter.test(player));
+            case "SEVERE" -> onlinePlayers.stream()
+                    .filter(player -> this.errorFilter.test(player));
+            default -> Stream.of();
+        };
+
+        return filteredPlayersStream.toList();
     }
 
     private @NonNull Component getStackTraceComponent(@NonNull Throwable throwable) {

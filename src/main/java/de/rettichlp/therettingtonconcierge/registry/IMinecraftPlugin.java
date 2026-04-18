04ac4895c50d2854091b0eb8020ee58496f41a68
RettichLP <rettichlp01@gmail.com>
2026-04-18 18:10:14 +0200

    Remove DevUtils and centralize plugin metadata handling in IMinecraftPlugin
    Co-authored-by: AI

--- src/main/java/de/rettichlp/therettingtonconcierge/registry/IMinecraftPlugin.java
+++ src/main/java/de/rettichlp/therettingtonconcierge/registry/IMinecraftPlugin.java
@@ -1,8 +1,17 @@
 package de.rettichlp.therettingtonconcierge.registry;
 
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
+import io.papermc.paper.plugin.configuration.PluginMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+
+import static de.rettichlp.therettingtonconcierge.registry.IMinecraftPlugin.PluginInformation.fromJavaPlugin;
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.Optional.ofNullable;
 
 public interface IMinecraftPlugin {
 
@@ -28,5 +37,49 @@ public interface IMinecraftPlugin {
         }
 
         return (JavaPlugin) this;
+    }
+
+    default PluginInformation getPluginInformation() {
+        if (isPaperPlugin()) {
+            return fromJavaPlugin(getPaperPlugin());
+        }
+
+        return ofNullable(this.getClass().getAnnotation(Plugin.class))
+                .map(PluginInformation::fromPluginAnnotation)
+                .orElse(null);
+    }
+
+    record PluginInformation(String id, String name, String version, String description, List<String> authors, List<String> contributors, String url, List<String> dependencies) {
+
+        public String getNiceVersion() {
+            return this.version.contains("-") ? this.version.split("-")[0] : this.version;
+        }
+
+        public static @NonNull PluginInformation fromJavaPlugin(@NonNull JavaPlugin javaPlugin) {
+            PluginMeta pluginMeta = javaPlugin.getPluginMeta();
+
+            String id = javaPlugin.namespace();
+            String name = javaPlugin.getName();
+            String version = pluginMeta.getVersion();
+            String description = pluginMeta.getDescription();
+            List<String> authors = pluginMeta.getAuthors();
+            List<String> contributors = pluginMeta.getContributors();
+            String url = pluginMeta.getWebsite();
+            List<String> dependencies = pluginMeta.getPluginDependencies();
+
+            return new PluginInformation(id, name, version, description, authors, contributors, url, dependencies);
+        }
+
+        public static @NonNull PluginInformation fromPluginAnnotation(@NonNull Plugin plugin) {
+            String id = plugin.id();
+            String name = plugin.name();
+            String version = plugin.version();
+            String description = plugin.description();
+            List<String> authors = asList(plugin.authors());
+            String url = plugin.url();
+            List<String> dependencies = stream(plugin.dependencies()).map(Dependency::id).toList();
+
+            return new PluginInformation(id, name, version, description, authors, authors, url, dependencies);
+        }
     }
 }
